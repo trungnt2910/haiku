3e9a03537429d4a2fce1236d35ac063ba838a580
X512 <danger_mail@list.ru>
2022-03-16 07:04:18 +0900

    libroot: implement Itanium ABI unwind functions by using LLVM libunwind
    Change-Id: Ic9e51cdbf89db91daf5d0cb62319e20c4c7c6c12

---
+++ src/system/libroot/unwind/libgcc.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include "config.h"
+
+_LIBUNWIND_EXPORT void __umoddi3() {abort();}
+_LIBUNWIND_EXPORT void __udivdi3() {abort();}
+_LIBUNWIND_EXPORT void __udivmoddi4() {abort();}
+_LIBUNWIND_EXPORT void __divdi3() {abort();}
+_LIBUNWIND_EXPORT void __moddi3() {abort();}
+_LIBUNWIND_EXPORT void __mulxc3() {abort();}
+_LIBUNWIND_EXPORT void __muldc3() {abort();}
+_LIBUNWIND_EXPORT void __mulsc3() {abort();}
+_LIBUNWIND_EXPORT void __divmoddi4() {abort();}
+_LIBUNWIND_EXPORT void __clz_tab() {abort();}
+_LIBUNWIND_EXPORT void __udivmodti4() {abort();}
+_LIBUNWIND_EXPORT void __udivti3() {abort();}
+_LIBUNWIND_EXPORT void __popcountdi2() {abort();}
